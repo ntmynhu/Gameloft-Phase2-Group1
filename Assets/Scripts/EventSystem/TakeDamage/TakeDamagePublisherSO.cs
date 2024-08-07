cac07a63042943258f31e39432283bb82eb68f01
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-08-07 23:45:08 +0800

    Add Event System

---
+++ Assets/Scripts/EventSystem/TakeDamage/TakeDamagePublisherSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New TakeDamage Pulisher", menuName = "Scriptable Objects/Events/TakeDamage Publisher")]
+public class TakeDamagePublisherSO : ScriptableObject
+{
+    public UnityAction<float, string, string> OnEventRaised;
+
+    public void RaiseEvent(float value, string tag, string myName)
+    {
+        OnEventRaised?.Invoke(value, tag, myName);
+    }
+}
