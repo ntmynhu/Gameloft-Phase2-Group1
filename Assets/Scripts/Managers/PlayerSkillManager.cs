cf603fa83c96157a7d690baa6be6ef507db59aa3
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-08-10 04:30:06 +0800

    Manage switching skill and set skill state

--- Assets/Scripts/Managers/PlayerSkillManager.cs
+++ Assets/Scripts/Managers/PlayerSkillManager.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerSkillManager : MonoBehaviour
 {
+    public Skill normal_skill; //static shoot
     public Skill skill_1;
     public Skill skill_2;
+    public List<Skill> skillList; //All skills stored here
+    public SkillActivator activator;
 
-    private static PlayerSkillManager instance;
-    public static PlayerSkillManager Instance => instance;
-
+  
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-        }    
-        else
-        {
-            Destroy(instance);
-        }
+        activator = GetComponent<SkillActivator>();
+        SetSkills(0, 1); //STATIC
+
+        
     }
 
+    private void Update()
+    {
+        if (activator.crnSkill == null)
+        {
+            activator.SetSkill(normal_skill, false);
+            this.gameObject.GetComponent<ShootProjectiles>().enabled = true;
+        }
+    }
+    private void Action_started_Skill1(InputAction.CallbackContext obj)
+    {
+        activator.SetSkill(skill_1);
+        this.gameObject.GetComponent<ShootProjectiles>().enabled = false;
+    }
+
+    private void Action_started_Skill2(InputAction.CallbackContext obj)
+    {
+        activator.SetSkill(skill_2);
+        this.gameObject.GetComponent<ShootProjectiles>().enabled = false;
+    }
+
+    public void SetSkills(int skill1ID, int skill2ID)
+    {
+        if (skill_1 != null)
+        {
+            skill_1.startAction.action.started -= Action_started_Skill1;
+        }
+        if (skill_2 != null)
+        {
+            skill_2.startAction.action.started -= Action_started_Skill2;
+        }
+        skill_1 = skillList.Find(skill => skill.id == skill1ID);
+        skill_2 = skillList.Find(skill => skill.id == skill2ID);
+        if (skill_1 != null)
+        {
+            skill_1.startAction.action.started += Action_started_Skill1;
+        }
+        if (skill_2 != null)
+        {
+            skill_2.startAction.action.started += Action_started_Skill2;
+        }
+    }    
     public void ReadySkill(Skill skill)
     {
         skill.SetReady();
